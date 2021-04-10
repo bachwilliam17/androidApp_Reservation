ac9532ca04357bee236ed2ad8d53c77a89d61457
skjoldurr <crlderoy21@gmail.com>
2021-04-10 14:50:32 -0400

    reservation confirmee vue CD

---
+++ AndroidProject/app/src/main/java/com/example/reservation/reservationConfirmee.java
@@ -0,0 +1,14 @@
+package com.example.reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reservationConfirmee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reservation_confirmee);
+    }
+}
